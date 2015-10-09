bba64ca737aa5b36cf50524d9331c1ed30d91185
emj-io <emj-io@users.noreply.github.com>
2015-10-08 22:04:04 -0700

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,4 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-ruby_install_ruby 'ruby 2.2'
+ruby_install_ruby 'ruby 2.0.0-p645'
+ruby_install_ruby 'ruby 2.1.6'
+ruby_install_ruby 'jruby 1.7.20'
+ruby_install_ruby 'rbx 2.5.5'
